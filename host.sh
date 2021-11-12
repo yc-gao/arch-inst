7f1de6db1f4ceddffeaf68bfa29b8c9ee4b25c6d
xundaoxd <xundaoxd@outlook.com>
2021-11-12 19:55:44 +0800

    Update host.sh

--- host.sh
+++ host.sh
@@ -4,9 +4,6 @@ systemctl stop reflector
 reflector --verbose --country China --protocol http --protocol https --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
 pacman -Syy
 
-mkfs.fat /dev/nvme0n1p1
-mkfs.btrfs -F /dev/nvme0n1p2
-
 mount /dev/nvme0n1p2 /mnt
 mkdir -p /mnt/boot/efi
 mount /dev/nvme0n1p1 /mnt/boot/efi
