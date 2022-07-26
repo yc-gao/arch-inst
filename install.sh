763acd265181074cbe816b2787ec280e58caf42b
yinchaogao <yinchaogao@deeproute.ai>
2022-07-26 15:41:18 +0800

    add sudo group for user

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ grub-mkconfig -o /boot/grub/grub.cfg
 pacman -S --noconfirm nvidia alsa-utils alsa-firmware pulseaudio pulseaudio-alsa pulseaudio-bluetooth
 
 useradd -m -s /bin/zsh $user
-usermod -aG wheel $user
+usermod -aG sudo $user
 
 pacman -S --noconfirm plasma kde-applications
 systemctl enable sddm
