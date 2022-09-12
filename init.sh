b2d39eac547900a838b41ecc2344b22793d69131
xundaoxd <xundaoxd@outlook.com>
2022-09-12 20:16:57 +0800

    install man

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+pacman -S --noconfirm man-db man-pages
+
 echo -e '[archlinuxcn]\nServer = https://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/$arch\n' >> /etc/pacman.conf \
     && pacman -Syy \
     && pacman -S --noconfirm archlinuxcn-keyring \
