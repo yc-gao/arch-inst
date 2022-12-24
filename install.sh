0ca07c58c942bfdc548e292e558f9106851174c7
xundaoxd <xundaoxd@outlook.com>
2022-12-24 21:21:27 +0800

    format

--- install.sh
+++ install.sh
@@ -1,69 +1,66 @@
 #!/bin/bash
 set -e
 
-prepare() {
-
-# systemctl stop reflector
-# reflector --verbose --country China --protocol http --protocol https --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
-
 esp_partition=/dev/nvme0n1p1
 root_partition=/dev/nvme0n1p2
-
-mkfs.ext4 -F ${root_partition}
-mkfs.fat -F 32 ${esp_partition}
-
-mount ${root_partition} /mnt
-mkdir -p /mnt/boot/efi
-mount ${esp_partition} /mnt/boot/efi
-
-pacstrap /mnt base base-devel linux-lts linux-firmware
-genfstab -U /mnt >> /mnt/etc/fstab
-
-cp install.sh /mnt/root/
-arch-chroot /mnt /root/install.sh install
-rm -rf  /mnt/root/install.sh
-
-}
-
-install() {
 
 hostname='xundaoxd-pc'
 user="xundaoxd"
 
-ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+prepare() {
+    # systemctl stop reflector
+    # reflector --verbose --country China --protocol http --protocol https --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
 
-echo -e 'en_US.UTF-8 UTF-8\nzh_CN.UTF-8 UTF-8' >> /etc/locale.gen
-echo 'LANG=en_US.UTF-8' > /etc/locale.conf
-locale-gen
+    mkfs.ext4 -F ${root_partition}
+    mkfs.fat -F 32 ${esp_partition}
 
-echo $hostname > /etc/hostname
+    mount ${root_partition} /mnt
+    mkdir -p /mnt/boot/efi
+    mount ${esp_partition} /mnt/boot/efi
 
-mkinitcpio -P
+    pacstrap /mnt base base-devel linux-lts linux-firmware
+    genfstab -U /mnt >> /mnt/etc/fstab
 
-pacman -S --noconfirm grub efibootmgr
-grub-install --efi-directory=/boot/efi --recheck
-grub-mkconfig -o /boot/grub/grub.cfg
+    cp install.sh /mnt/root/
+    arch-chroot /mnt /root/install.sh install
+    rm -rf  /mnt/root/install.sh
+    umount -R /mnt
+}
 
-pacman -S --noconfirm nvidia-lts alsa-utils alsa-firmware pulseaudio pulseaudio-alsa pulseaudio-bluetooth
+install() {
+    ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
-pacman -S --noconfirm plasma kde-applications fcitx-googlepinyin kcm-fcitx
-systemctl enable sddm
-systemctl enable NetworkManager
-systemctl enable bluetooth
+    echo -e 'en_US.UTF-8 UTF-8\nzh_CN.UTF-8 UTF-8' >> /etc/locale.gen
+    echo 'LANG=en_US.UTF-8' > /etc/locale.conf
+    locale-gen
 
-pacman -Syy
-pacman -S --noconfirm zsh git neovim python-pynvim firefox openssh wget
+    echo $hostname > /etc/hostname
 
-useradd -m -s /bin/zsh $user
-usermod -aG wheel $user
-EDITOR=nvim visudo
+    mkinitcpio -P
 
-echo 'set root password.'
-passwd
+    pacman -S --noconfirm grub efibootmgr
+    grub-install --efi-directory=/boot/efi --recheck
+    grub-mkconfig -o /boot/grub/grub.cfg
 
-echo "set $user password."
-passwd $user
+    pacman -S --noconfirm nvidia-lts alsa-utils alsa-firmware pulseaudio pulseaudio-alsa pulseaudio-bluetooth
 
+    pacman -S --noconfirm plasma kde-applications fcitx-googlepinyin kcm-fcitx
+    systemctl enable sddm
+    systemctl enable NetworkManager
+    systemctl enable bluetooth
+
+    pacman -Syy
+    pacman -S --noconfirm zsh git neovim python-pynvim firefox openssh wget
+
+    useradd -m -s /bin/zsh $user
+    usermod -aG wheel $user
+    EDITOR=nvim visudo
+
+    echo 'set root password.'
+    passwd
+
+    echo "set $user password."
+    passwd $user
 }
 
 if [[ $# -eq 1 ]]; then
