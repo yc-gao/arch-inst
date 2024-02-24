2ddc8bfe8759f5fcad58146f2eda8964de40c5ed
xundaoxd <xundaoxd@outlook.com>
2024-02-24 06:09:19 +0000

    fix issue

--- profiledef.sh
+++ profiledef.sh
@@ -7,7 +7,9 @@ iso_publisher="xundaoxd <https://github.com/xundaoxd>"
 iso_application="Arch Linux Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
-bootmodes=('uefi-ia32.grub.esp' 'uefi-x64.grub.esp')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
+           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
+           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
