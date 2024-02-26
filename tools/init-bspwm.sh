5ad911d8860cda5140f4540714d06bcc3c303512
yinchaogao <yinchaogao@deeproute.ai>
2024-02-26 13:35:35 +0800

    update

--- tools/init-bspwm.sh
+++ tools/init-bspwm.sh
@@ -4,7 +4,7 @@ set -e
 self_dir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 proj_dir=$(dirname "$self_dir")
 
-wdir="$HOME/Workdir"
+opt_wdir="$HOME/Workdir"
 user="xundaoxd"
 
 die() {
@@ -28,6 +28,7 @@ docker() {
     if [[ $UID != 0 ]]; then
         run_asroot docker
         yay -S --noconfirm nvidia-container-toolkit
+        rm -rf ~/go
         return
     fi
     pacman -Syy
@@ -36,25 +37,14 @@ docker() {
     usermod -aG docker $user
 }
 
-virt() {
-    [[ $UID != 0 ]] && run_asroot virt && return
-    pacman -Syy
-    pacman -S --noconfirm virt-manager dnsmasq qemu-full \
-        && systemctl enable libvirtd \
-        && usermod -aG libvirt,kvm $user \
-        && sed -i '/^unix_sock_group/{s/#//}' /etc/libvirt/libvirtd.conf
-}
-
 custom() {
-    git clone git@github.com:xundaoxd/dotfiles.git "$wdir/dotfiles"
-    (cd "$wdir/dotfiles" && ./install.sh -f)
-    rm -rf ~/go
+    git clone git@github.com:xundaoxd/dotfiles.git "${opt_wdir}/dotfiles"
+    (cd "${opt_wdir}/dotfiles" && ./install.sh -f)
 }
 
 bspwm_desktop() {
     if [[ $UID != 0 ]]; then
         run_asroot bspwm_desktop
-        yay -S --noconfirm daemonize
         cp -r "${proj_dir}/airootfs/home/xundaoxd" /home/
         return
     fi
@@ -69,7 +59,8 @@ bspwm_desktop() {
 
     pacman -S --noconfirm vlc evince firefox obsidian \
         usbutils ffmpeg \
-        man-db man-pages wget curl xclip ripgrep-all ctags openbsd-netcat unzip neovim jq nmap rsync
+        man-db man-pages wget curl xclip ripgrep-all \
+        ctags openbsd-netcat unzip neovim jq nmap rsync
 
     cp -r "${proj_dir}/airootfs/etc/modprobe.d" /etc/
     cp "${proj_dir}/airootfs/etc/sddm.conf" /etc/
@@ -81,18 +72,20 @@ bspwm() {
 
     aur
     docker
-    virt
     bspwm_desktop
     custom
 }
 
-while getopts 'w:' opt; do
-    case $opt in
-        w)      wdir="$OPTARG";;
-        ?)      die "undefined opt: $opt";
+while (($#)); do
+    case $1 in
+        -w)
+            opt_wdir="$2"
+            shift 2
+            ;;
+        *)
+            break
+            ;;
     esac
 done
-shift $((OPTIND - 1))
-
 bspwm "$@"
 
