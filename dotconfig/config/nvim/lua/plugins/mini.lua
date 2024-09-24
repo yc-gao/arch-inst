6c35130f0b9b31ac7a7437e6df2004f2e6f3dfb6
ycgao <yc_x@outlook.com>
2024-09-24 23:38:18 +0800

    update

--- dotconfig/config/nvim/lua/plugins/mini.lua
+++ dotconfig/config/nvim/lua/plugins/mini.lua
@@ -2,7 +2,6 @@ return {
     'echasnovski/mini.nvim',
     version = '*',
     config = function()
-        require('mini.cursorword').setup()
         require('mini.pairs').setup()
         require('mini.align').setup()
     end,
