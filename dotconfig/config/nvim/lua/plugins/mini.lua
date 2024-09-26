1f2bca24b7e3b59b111f352bf147a0caea7d5f2b
yinchaogao <yinchaogao@deeproute.ai>
2024-09-26 14:19:17 +0800

    update nvim

--- dotconfig/config/nvim/lua/plugins/mini.lua
+++ dotconfig/config/nvim/lua/plugins/mini.lua
@@ -3,7 +3,6 @@ return {
     version = '*',
     config = function()
         require('mini.pairs').setup()
-        require('mini.comment').setup()
         require('mini.move').setup()
         require('mini.align').setup()
     end,
