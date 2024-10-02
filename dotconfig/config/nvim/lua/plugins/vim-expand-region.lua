713f72c77ed248e8bd40588597b4cd7506aa5169
ycgao <yc_x@outlook.com>
2024-10-03 01:15:57 +0800

    update nvim

--- dotconfig/config/nvim/lua/plugins/vim-expand-region.lua
+++ dotconfig/config/nvim/lua/plugins/vim-expand-region.lua
@@ -5,9 +5,6 @@ return {
         { '<A-->', '<Plug>(expand_region_shrink)', { 'n', 'v' } },
     },
     config = function()
-        vim.keymap.del({ 'n', 'v' }, '+')
-        vim.keymap.del({ 'n', 'v' }, '_')
-
         vim.keymap.set({ 'n', 'v' }, '<A-->', '<Plug>(expand_region_shrink)')
         vim.keymap.set({ 'n', 'v' }, '<A-=>', '<Plug>(expand_region_expand)')
     end
