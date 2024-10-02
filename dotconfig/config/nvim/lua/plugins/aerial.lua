fa5b55c2a73d46c15aa5b77ea57568d65bea5c15
ycgao <yc_x@outlook.com>
2024-10-02 22:37:48 +0800

    add aerial

---
+++ dotconfig/config/nvim/lua/plugins/aerial.lua
@@ -0,0 +1,31 @@
+return {
+    'stevearc/aerial.nvim',
+    -- Optional dependencies
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter',
+        'nvim-tree/nvim-web-devicons'
+    },
+    config = function()
+        require('aerial').setup({
+            backends = { 'treesitter', 'lsp', 'markdown', 'asciidoc', 'man' },
+            layout = {
+                max_width = { 50, 0.3 },
+                min_width = 20,
+            },
+            keymaps = {
+                ['<CR>'] = 'actions.jump',
+                ['<2-LeftMouse>'] = 'actions.jump',
+
+                ['l'] = 'actions.tree_open',
+                ['L'] = 'actions.tree_toggle_recursive',
+                ['h'] = 'actions.tree_close',
+                ['H'] = 'actions.tree_close_recursive',
+
+                ['<A-j>'] = 'actions.down_and_scroll',
+                ['<A-k>'] = 'actions.up_and_scroll',
+            },
+        })
+
+        vim.keymap.set('n', '<leader>co', '<cmd>AerialToggle<CR>')
+    end,
+}
