292c839bbf145aeddb3b9e57b9f77b26c8f2548f
yc_x <yc_x@outlook.com>
2024-12-15 13:06:55 +0800

    update nvim-cmp mapping

--- dotconfig/config/nvim/lua/plugins/nvim-cmp.lua
+++ dotconfig/config/nvim/lua/plugins/nvim-cmp.lua
@@ -48,6 +48,12 @@ local mason_init = function()
     })
 end
 
+local has_words_before = function()
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 local cmp_init = function()
     local luasnip = require('luasnip')
     require('luasnip.loaders.from_vscode').lazy_load()
@@ -64,19 +70,21 @@ local cmp_init = function()
         },
         mapping = cmp.mapping.preset.insert({
             ['<Tab>'] = function(fallback)
-                if luasnip.locally_jumpable(1) then
-                    luasnip.jump(1)
-                elseif cmp.visible() then
+                if cmp.visible() then
                     cmp.select_next_item()
+                elseif luasnip.locally_jumpable(1) then
+                    luasnip.jump(1)
+                elseif has_words_before() then
+                    cmp.complete()
                 else
                     fallback()
                 end
             end,
             ['<S-Tab>'] = function(fallback)
-                if luasnip.locally_jumpable(-1) then
-                    luasnip.jump(-1)
-                elseif cmp.visible() then
+                if cmp.visible() then
                     cmp.select_prev_item()
+                elseif luasnip.locally_jumpable(-1) then
+                    luasnip.jump(-1)
                 else
                     fallback()
                 end
