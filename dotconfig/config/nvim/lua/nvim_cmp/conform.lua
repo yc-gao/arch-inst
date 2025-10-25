e776f9f53227b96a0c725e9f5440a8d986ec8829
ycgao <yc_x@outlook.com>
2025-10-25 11:40:02 +0800

    update

--- dotconfig/config/nvim/lua/nvim_cmp/conform.lua
+++ dotconfig/config/nvim/lua/nvim_cmp/conform.lua
@@ -2,6 +2,8 @@ local conform = require('conform')
 conform.setup({
     formatters_by_ft = {
         cpp = {'clang-format'},
+        c = {'clang-format'},
+        sh = {'shfmt'},
     },
 })
 vim.keymap.set('n', 'gq', function() conform.format() end)
